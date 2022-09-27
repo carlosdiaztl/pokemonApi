0483b56ac8014061a750fc8882a8230d62bccfc5
camilon0 <kmilo8433@gmail.com>
2022-09-26 20:00:33 -0500

    sincambios

--- main.js
+++ main.js
@@ -2,20 +2,20 @@ import { getDataUnique, getInfo } from "./scripts/process.js";
 import { printPokemons } from "./scripts/ui.js";
 
 const mainFunction = async () => {
-    let data = await getInfo();
-    let responseInfo = [];
-    data.forEach(element => {
-        let infoPokemons = getDataUnique(element.url);
-        responseInfo.push(infoPokemons)
-    });
+  let data = await getInfo();
+  let responseInfo = [];
+  data.forEach((element) => {
+    let infoPokemons = getDataUnique(element.url);
+    responseInfo.push(infoPokemons);
+  });
 
-    const newResponse = await Promise.all(responseInfo);
+  const newResponse = await Promise.all(responseInfo);
 
-    data.forEach((_, index) => {
-        data[index].info = newResponse[index];
-    })
-    data.splice(6,14)
-    printPokemons(data);
-}
+  data.forEach((_, index) => {
+    data[index].info = newResponse[index];
+  });
+  data.splice(6, 14);
+  printPokemons(data);
+};
 
 mainFunction();
--- scripts/process.js
+++ scripts/process.js
@@ -1,16 +1,16 @@
 export const getInfo = async () => {
-    const URL = 'https://pokeapi.co/api/v2/pokemon/';
-    const { data: { results } } = await axios.get(URL);
-    // results.forEach(async (pokemon, index) => {
-    //     const {data} = await axios.get(pokemon.url)
-    //     results[index].info = data
-    // });
-    return results;
-}
+  const URL = "https://pokeapi.co/api/v2/pokemon/";
+  const {
+    data: { results },
+  } = await axios.get(URL);
+  // results.forEach(async (pokemon, index) => {
+  //     const {data} = await axios.get(pokemon.url)
+  //     results[index].info = data
+  // });
+  return results;
+};
 
 export const getDataUnique = async (url) => {
-    const {data} = await axios.get(url)
-    return data;
-}
-
-
+  const { data } = await axios.get(url);
+  return data;
+};

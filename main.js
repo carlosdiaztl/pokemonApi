8bf8c0b56d6ede0d0ff7d6022725e6bf2d06f4cd
camilon0 <kmilo8433@gmail.com>
2022-09-26 20:01:53 -0500

    filtro por nombre sin pintar

--- main.js
+++ main.js
@@ -2,21 +2,26 @@ import { getDataUnique, getInfo } from "./scripts/process.js";
 import { printPokemons } from "./scripts/ui.js";
 
 const mainFunction = async () => {
-    let data = await getInfo();
-    let responseInfo = [];
-    data.forEach(element => {
-        let infoPokemons = getDataUnique(element.url);
-        responseInfo.push(infoPokemons)
-    });
+  let data = await getInfo();
+  let responseInfo = [];
+  data.forEach((element) => {
+    let infoPokemons = getDataUnique(element.url);
+    responseInfo.push(infoPokemons);
+  });
 
-    const newResponse = await Promise.all(responseInfo);
+  const newResponse = await Promise.all(responseInfo);
 
-    data.forEach((_, index) => {
-        data[index].info = newResponse[index];
-    })
-    data.splice(6,14)
-    printPokemons(data);
-}
+  data.forEach((_, index) => {
+    data[index].info = newResponse[index];
+  });
+  data.splice(6, 14);
+  filterArray("bulbasaur", data);
+};
 
 mainFunction();
 // nueva funcion
+
+const filterArray = (text, list) => {
+  let query = list.filter((element) => element.name === text);
+  console.log(query);
+};
